22cc60650924c738889ea4375c72873aedc48abe
William Luck <luck.william.h@gmail.com>
2023-09-07 12:27:23 -0300

    add python post request for last ten news stories

--- main.py
+++ main.py
@@ -13,15 +13,25 @@ last_ten = []
 prompt_message = 'The following is a list, that repeats three sets of information related to a news story, containing title, the content, and link to the news story, int that order. Condense this information into a briefing format to be read by someone in five minutes or less. When necessary, provide the links or further information for context on specific stories. Think about: What should the reader know about to better understand these stories? What information should they read more about if they are interested it? What could be the impact of these new stories? Why is this news important?' 
 
 for article in articles:
-    last_ten.append(article.h3.text.strip())
-    last_ten.append(article.p.text.strip())
-    last_ten.append(f"reuters.com{article.find('div', class_ = 'story-content').a['href']}")
+    article_info = {}
+
+    article_info['title'] = article.h3.text.strip()
+    article_info['content'] = article.p.text.strip()
+    article_info['link'] = f"reuters.com{article.find('div', class_ = 'story-content').a['href']}"
+    # last_ten.append(article.h3.text.strip())
+    # last_ten.append(article.p.text.strip())
+    # last_ten.append(f"reuters.com{article.find('div', class_ = 'story-content').a['href']}")
+    last_ten.append(article_info)
 
 
-last_ten.insert(0, prompt_message)
+# last_ten.insert(0, prompt_message)
 
 
 
+for article in last_ten: 
+    print(article)
+    print('')
+    requests.post('http://localhost:3000/posts', json = article)
 
 
 
